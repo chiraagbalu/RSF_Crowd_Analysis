7f77c71b9f7c1dea22920380741f06366996b627
Chiraag Balu <chiraagbalu@berkeley.edu>
2022-07-28 19:30:44 -0700

    rsf_alert.py, script that does the magic

---
+++ rsf_alert.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from requests_html import HTMLSession
+import time
+from discord import Webhook, RequestsWebhookAdapter
+import config
+
+
+# DISCORD WEBHOOK SETUP
+
+# set up webhook using the url in config.py (not given in repo - private data)
+webhook = Webhook.from_url(config.webhookurl, adapter=RequestsWebhookAdapter())
+# send test message to url
+#webhook.send("Hello World")
+
+
+# url to read from: this is where RSF crowd meter reads from i believe its an API endpoint
+rsf_url = 'https://safe.density.io/#/displays/dsp_956223069054042646?token=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e'
+
+# making a dataframe (why am i doing this part again)
+df = pd.DataFrame(columns={'timestamp', 'fullness'})
+df = df[['timestamp', 'fullness']]
+
+# new session for html
+session = HTMLSession()
+
+# writes data, infinite loop
+# planning to daemonize this code to run forever on an EC2 instance or something
+# would also like to not have to host it in some random df
+
+# todo: make the bot send alerts to separate roles using discord webhook
+# different roles for different threshold/timeout values
+# currently only one setting
+# better messages
+
+# another idea: use regression or smth to predict rsf crowd for the next 15/30min
+
+# todo: time check, don't send alerts when the RSF isn't open
+
+
+def sendAlert(timeout=900, threshold=50):
+
+    # running loop
+    while True:
+        # get url of html page to load
+        r = session.get(rsf_url)
+        # render the page, let it load (sleep = 5)
+        r.html.render(sleep=5)
+        # look at the spot we need the data from
+        info = r.html.find('div.styles_fullness__rayxl')
+        # reads as list of elements, translate to text
+        newlist = [item.text for item in info]
+        # error catch if there's nothing there
+        if len(newlist) == 0:
+            # send alert if smth wrong w data
+            webhook.send('no data')
+            continue
+        # get the number from the thing
+        result = int(newlist[0].split('%')[0])
+        # condition for sending different messages
+        if result > threshold:
+            webhook.send(
+                f'rsf fullness above threshold of {threshold}%: maybe wait a bit')
+            webhook.send(f'rsf is {result}% full')
+        else:
+            # webhook.send(f'@everyone')
+            webhook.send(
+                f'rsf fullness below threshold of {threshold}%: go now!')
+            webhook.send(f'rsf is {result}% full')
+
+        # wait extra time: in seconds
+        time.sleep(timeout)
+
+# figure out what to do about writing to database and stuff lol
+# probably write to postgres or smth and then have data analysis w pandas/matplotlib/smth pretty -> host analysis using some frontend stuff for a cute website
+
+
+def writeData(timeout=0):
+    while True:
+        r = session.get(rsf_url)
+        r.html.render(sleep=5)
+        info = r.html.find('div.styles_fullness__rayxl')
+        newlist = [item.text for item in info]
+        if len(newlist) == 0:
+            print('no data')
+            continue
+        result = int(newlist[0].split('%')[0])
+        now = pd.Timestamp.now()
+        now_ts = now.timestamp()
+        df.loc[len(df.index)] = [now_ts, result]
+        print(df)
+        time.sleep(timeout)
+
+
+sendAlert(threshold=0)
